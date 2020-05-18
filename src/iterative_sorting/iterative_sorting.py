14f953f0fd4513c056d59f7e868524d24c36d4a5
Michael Stevens <stevensmike36@yahoo.com>
2020-05-18 16:32:39 -0500

    mvp

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,25 +1,54 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
     # loop through n-1 elements
+    # n -1 because after the sort, the one remaining is the largest
     for i in range(0, len(arr) - 1):
+        print(arr)
         cur_index = i
         smallest_index = cur_index
+        
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+        # (hint, can do in 3 loc) 
+        # use first number in list and 
+        #loop over right side starting with the next index 
+             
+        for next_smallest in range(cur_index, len(arr)):
+            
+        # is next smallest less than right side of arr--smallest index
+            
+            if arr[next_smallest] < arr[smallest_index]:
+                
+        # make new index equal to smallest index
+        
+                smallest_index =  next_smallest
+                           
+        # TO-DO: swap a,b = b,a 
+        
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
-
-        # TO-DO: swap
-        # Your code here
-
-    return arr
+    return arr #make sure you are returning in the proper place
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
+    #need to know if swaps occurred
+    swaps = True
+    #for pair in range(len(arr))
+    #should be using a while loop
+    while swaps:
+    #loop through elements 
+        swaps = False
+        for i in range(len(arr) - 1):
+            pair = i
 
+    #compare elements if arr[i] is greater move right
+    
+            if arr[pair] > arr[pair + 1]:
+                
+            #swap them a,b = b,a
 
+                arr[pair], arr[pair + 1] = arr[pair + 1], arr[pair]
+                swaps = True
     return arr
 
 
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,7 @@
 def linear_search(arr, target):
-    # Your code here
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
 
 
     return -1   # not found
@@ -8,7 +10,43 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    if len(arr) == 0:
+        return -1 # array empty
 
+    low = 0   #left
+    high = len(arr)-1 #right
 
-    return -1  # not found
+    while low <= high:
+        mid = (low + high) //2
+    
+        if arr[mid] == target:
+            return mid
+        
+        if arr[mid] < target:
+        #toss out the left side of arr
+        #update our left index
+            low = mid + 1      
+        
+        #otherwise, arr[mid] > target  
+        else:
+            high = mid - 1
+
+    return -1 # not found
+#Recursive Binary search  
+# def binary_search_recursive(arr, target, low, high):
+  
+#     middle = (low+high)//2
+
+    # if len(arr) == 0:
+    #     return -1 # array empty
+    
+    #     if arr[middle] == target:
+    #         return middle
+        
+    #     elif arr[middle] > target:
+    #         return binary_search_recursive(arr, target, low, high - 1)
+    #     else:
+    #         return binary_search_recursive(arr, target, low + 1, high)
+    
+    # else:
+    #     return - 1
